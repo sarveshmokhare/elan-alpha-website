d0b0c35f706dd21c63fed8e72709d685387df8dd
Himanshu Kumar Gupta <ai21btech11012@gmail.com>
2022-12-08 22:47:44 +0530

    doing changes in events page

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -6,13 +6,54 @@ import '../styles/events.css'
 function Events() {
   const context = useContext(ThemeContext)
 
+
+
   return (
     <div id={context.theme}>
-      <h1 className='heading text-purple-700 text-sm'>
+      <h1 className='heading'>
         Events
       </h1>
+      <div id="options">
+        <button className="pronites active" onClick={click}>Pronites</button>
+        <button onClick={click} className="workshops">Workshops</button>
+        <button onClick={click} className="informals" >Informals</button>
+      </div>
+      <div id="content">
+        <div id="pronites" className="pronites active">
+          <div className='sub_heading' >
+            PRONITES
+          </div>
+        </div>
+        <div className="workshops">
+          <div className='sub_heading' >
+            WORKSHOPS
+          </div>
+        </div>
+        <div className="informals">
+          <div className='sub-heading' >
+            INFORMALS
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
+
+function click(event){
+
+  var classname=event.target.className
+
+  console.log(classname)
+
+  document.getElementById("sub_heading").getElementsByClassName("active")[0].classList.remove("active")
+
+  event.target.classList.add("active")
+
+  document.getElementById("content").getElementsByClassName("active")[0].classList.remove("active")
+
+  document.getElementById("content").getElementsByClassName(classname)[0].classList.add("active")
+  
+}
+
 
 export default Events
